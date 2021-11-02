51d69c16690d2583e05f3e2bbaa40a1bfbb061dd
murch25 <31939550+murch25@users.noreply.github.com>
2021-11-02 17:27:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def hello():
-    print("hi")
-  def bye():
-    print("bye")
- print(hello())
+# An integer assignment
+age = 45
+
+# A floating point
+salary = 1456.8
+
+# A string
+name = "John"
+
+print(age)
+print(salary)
+print(name)
